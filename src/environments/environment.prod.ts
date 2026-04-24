93728b60b8f793c5008aebc757c2eed80b7e3770
Simeon <gangosimeon@gmail.com>
2026-04-24 16:57:49 +0000

    sup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:4000/api',
-  // apiUrlFront: 'https://votre-frontend.vercel.app',
+  // apiUrl: 'https://easyorder-backend-wnku.onrender.com',
 };
