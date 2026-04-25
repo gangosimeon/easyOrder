719612d95dc21cb14daa34f51089c645e058f613
Simeon <gangosimeon@gmail.com>
2026-04-25 10:06:44 +0000

    add api to url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:4000/api',
-  apiUrl: 'https://easyorder-backend-wnku.onrender.com',
+  apiUrl: 'http://localhost:4000/api',
+  // apiUrl: 'https://easyorder-backend-wnku.onrender.com',
 };
